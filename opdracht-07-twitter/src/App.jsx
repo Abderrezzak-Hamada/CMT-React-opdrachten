14d7aceed41ed4004ed094bc9a218adcd36c90b1
Abderrezzak-Hamada <9024905@student.zadkine.nl>
2025-11-26 12:08:40 +0100

    gg

--- opdracht-07-twitter/src/App.jsx
+++ opdracht-07-twitter/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import CreateMessage from "./components/CreatMessage";
+import MessageList from "./components/MessageList";
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+  const [message, setMessage] = useState([]);
+
+  let name = "Abderrezzak";
 
   return (
     <>
-      <h1>Opdracht 7 - Twitter</h1>
+      <CreateMessage
+        textInput={textInput}
+        setTextInput={setTextInput}
+        message={message}
+        setMessage={setMessage}
+      />
+
+      <MessageList name={name} message={message} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
